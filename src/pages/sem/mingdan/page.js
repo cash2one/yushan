1fa55afb45cf694091f85424c4aba88dcb21c8be
1578388951 <1578388951@qq.com>
2016-12-27 10:38:38 +0800

    add黑名单

---
+++ src/pages/sem/mingdan/page.js
@@ -0,0 +1,85 @@
+/**
+ * Created by jy on 2016/12/6.
+ */
+/*eslint-disable */
+require('cp');
+
+
+require('static/css/reset.css');
+require('static/vendor/Tabullet');
+require('./page.css');
+
+const eventBus = require('static/js/eventBus');
+const store = require('static/js/store');
+const utils = require('utils');
+const apiUrl = require('static/js/api');
+
+let currentAccount = store.getCurrentAccount();
+
+eventBus.on('account_change', function () {
+  currentAccount = store.getCurrentAccount();
+
+});
+function resetTabullet() {
+  $("#table").tabullet({
+    data: source,
+    action: function(mode, data) {
+      console.dir(mode);
+      if (mode === 'save') {
+        source.push(data);
+      }
+      if (mode === 'edit') {
+        for (var i = 0; i < source.length; i++) {
+          if (source[i].id == data.id) {
+            source[i] = data;
+          }
+        }
+      }
+      if (mode == 'delete') {
+        for (var i = 0; i < source.length; i++) {
+          if (source[i].id == data) {
+            source.splice(i, 1);
+            break;
+          }
+        }
+      }
+      resetTabullet();
+    }
+  });
+}
+
+$(() => {
+  /*utils.ajax(apiUrl.getApiUrl('getSKWord'), {
+    appid: $('.appid').val(),
+    date: $('.time').val(),
+  }).done(function (data) {
+    datas = data;
+    $('#tb').html(tb({ data: datas }));
+    $('#myTable').tablesorter();
+  });*/
+  $(function() {
+    var source = [{
+      id: 1,
+      name: "Aditya Purwa",
+      level: "Admin"
+    }, {
+      id: 2,
+      name: "Aditya Avaga",
+      level: "Manager"
+    }, {
+      id: 101,
+      name: "Aditya Myria",
+      level: "User"
+    }, {
+      id: 102,
+      name: "Lucent Serentia",
+      level: "LOD"
+    }, {
+      id: 103,
+      name: "Hayden Bapalthiel",
+      level: "King"
+    }, ];
+
+    resetTabullet();
+  });
+});
