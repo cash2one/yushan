15bb72fe6df6bd6008203158e422a42d22222c81
1578388951 <1578388951@qq.com>
2017-01-06 13:55:30 +0800

    更改日报5

--- src/pages/sem/bili/page.js
+++ src/pages/sem/bili/page.js
@@ -20,11 +20,15 @@ const eventBus = require('static/js/eventBus');
 const store = require('static/js/store');
 const utils = require('utils');
 const apiUrl = require('static/js/api');
-
+let flo=1;
 let currentAccount = store.getCurrentAccount();
 
 eventBus.on('account_change', function () {
   currentAccount = store.getCurrentAccount();
+  window.location.reload()
+  /*flo=0;
+  $('.trends-all').empty();
+  $('.tol').empty();*/
 });
 
 function lin(who,date1,date2){
@@ -35,15 +39,79 @@ function lin(who,date1,date2){
     date2: date2,
   }).done(function (data) {
     console.log(data);
-    /*const myChart = echarts.init(document.getElementById(who));
+    let days=[];
+    let series=[];
+    let datas=[];
+    let cpc=[];
+    let show=[];
+    let click=[];
+    let spend=[];
+    for(let i=0;i<data.length;i++){
+      days.push(utils.dateFormat(data[i].dateTime, 'MM-dd hh:mm'));
+      cpc.push(data[i].cpc.toFixed(2));
+      show.push(data[i].showCount);
+      click.push(data[i].clickCount);
+      spend.push(data[i].spend);
+    }
+    // console.log(cpc);
+    // console.log(show);
+    // console.log(click);
+    // console.log(spend);
+    datas[0]={
+      name: 'CPC',
+      data: cpc,
+    };
+    datas[1]={
+      name: '点击',
+      data: click,
+    };
+    datas[2]={
+      name: '展现',
+      data: show,
+    };
+    datas[3]={
+      name: '消费',
+      data: spend,
+    };
+    console.log(datas);
+    for(let i=0;i<datas.length;i++){
+      series.push({
+        name: datas[i].name,
+        type: 'line',
+        line: '总量',
+        data: datas[i].data,
+        symbol: 'emptyCircle',
+        symbolSize: 3,
+        smooth: true,
+        itemStyle: {
+          normal: {
+            lineStyle: { // 系列级个性化折线样式，横向渐变描边
+              width: 2,
+            },
+          },
+          emphasis: {
+            label: {
+              show: true,
+            },
+          },
+        },
+      });
+    }
+
+    // console.log(days);
+    const myChart = echarts.init(document.getElementById(who));
     const option = {
       tooltip: {
         trigger: 'axis',
       },
+      grid: {
+        show:true,
+        borderWidth:'1',
+        borderColor:'black'
+      },
       legend: {
         orient: 'horizontal',
-        selected: selected1,
-        data: legendData1,
+        data:['CPC', '展现', '点击', '消费'],
       },
       toolbox: {
         show: true,
@@ -72,7 +140,7 @@ function lin(who,date1,date2){
         {
           type: 'category',
           boundaryGap: false,
-          data: dates,
+          data: days,
           splitLine: {
             show: true,
             lineStyle: {
@@ -108,63 +176,30 @@ function lin(who,date1,date2){
               width: 0,
             },
           },
-        },
-        {
-          type: 'value',
-          splitNumber: 10,
-          axisLabel: {
-            show: true,
-            interval: 'auto', // {number}
-            margin: 18,
-            textStyle: {
-              color: '#999999',
-              fontFamily: 'verdana',
-              fontSize: 12,
-              fontStyle: 'normal',
-              fontWeight: 'bold',
-            },
-            formatter: function (value) {
-              return value + '%';
-            },
-          },
-          splitLine: {
-            show: true,
-            lineStyle: {
-              color: '#483d8b',
-              type: 'dotted',
-              width: 0,
-            },
-          },
-        },
+        }
       ],
       series: series,
     };
     myChart.setOption(option);
-    window.onresize = myChart.resize;*/
+    window.onresize = myChart.resize;
   });
 
 }
 $(() => {
   // let $cln=$('.trends');
-  let flo=1;
-  $('.date1').attr('value', utils.getDateStr(-2));
+  $('.date1').attr('value', utils.getDateStr(-1));
   $('.dp').datepicker();
   $(document).on('click','.del',function(){
     $(this).parent().remove();
   });
-  $('#date1').change(function () {
-    var arr1 = $('#date1').val().split('/');
-    var arr2 = $('#date2').val().split('/');
-    var s = arr1[2] + '-' + arr1[0] + '-' + arr1[1];
-    var e = arr2[2] + '-' + arr2[0] + '-' + arr2[1];
-  });
   $('.tj').click(function(){
     $('.trends-all').append( mo());
-    $(".dp").datepicker();
-    $('.date1').attr('value', utils.getDateStr(-2));
-  })
+    $('.dp').datepicker();
+    $('.date1').attr('value', utils.getDateStr(-1));
+  });
 
   $('.ck').click(function(){
+    $('.tol').empty();
     $('.trends').each(function(){
       flo++;
       let index='d'+flo;
